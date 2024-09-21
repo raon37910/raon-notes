e4409dd36141d55236d1c3ff365e8153c72f9e4c
김정민 <raon37910@gmail.com>
2024-09-21 12:59:20 +0900

    🔧 [formatting] Prettier 룰 추가

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,6 +2,15 @@
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
-const config = {};
+const config = {
+  useTabs: false,
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "all",
+  endOfLine: "lf",
+  semi: false,
+  arrowParens: "always",
+};
 
 export default config;
